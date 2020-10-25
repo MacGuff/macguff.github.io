53174e83307e188aa0dae77d2382e0b088f22e73
macguff <974123274@qq.com>
2020-10-25 23:04:47 +0800

    add deploy script

---
+++ deploy-pages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+abp && git checkout master
+/bin/cp -rf dist/* .
+git add .
+git commit -m "site updated: $(date '+%F %T')"
+git push origin master
+
+[ $? -eq 0 ] && echo "部署至gh-pages成功！"
+git checkout src
